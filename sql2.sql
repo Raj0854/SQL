cd9c2756223dbdcc27543ee1c04d731eb9afd5bf
Raj Gupta <rajgupta.02.0854@gmail.com>
2026-03-19 11:19:13 +0530

    Add files via upload

--- sql2.sql
+++ sql2.sql
@@ -44,3 +44,6 @@ select count(country) from customers where city = "NYC";
 select * from customers where country = "USA" and creditlimit > 100000;
 -- when one condtion are true use or operator
 select * from customers where country = "USA" or city = "NYC";
+select * from customers where country = "USA" and creditlimit = "0";
+select * from customers where  creditlimit > "50000" and creditlimit < "100000";
+select count(creditlimit) from customers where  creditlimit > "50000" and creditlimit < "100000";
