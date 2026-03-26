2c3bd080f69e6f3d1d0054ad0e15f0515ef02840
Raj Gupta <rajgupta.02.0854@gmail.com>
2026-03-26 11:02:20 +0530

    Add files via upload

--- sql3.sql
+++ sql3.sql
@@ -19,3 +19,30 @@ price int,
 stock int default 0
 );
 select * from books;
+-- 26 march 2026
+-- update query
+show databases;
+use classicmodels;
+show tables;
+select*from customers;
+update customers set creditlimit = 30000 where customerNumber =103;
+
+-- delete records
+-- delete from customers where customerNumber =103;
+
+-- like 
+select* from customers where city like "m%";
+select* from customers where state  like "n%";
+
+-- top list (limit)
+select* from customers limit 15;
+
+-- order by
+select* from customers order by customerNumber asc;
+select* from customers order by customerNumber desc;
+
+select * from customers order by creditlimit asc limit 20;
+select * from customers order by creditlimit desc limit 20;
+
+
+
