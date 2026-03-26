87d6583dc8211a75be60417c274a6eb3af315b5e
Raj Gupta <rajgupta.02.0854@gmail.com>
2026-03-26 14:44:51 +0530

    Add files via upload

--- assignment sql practice.sql
+++ assignment sql practice.sql
@@ -5,7 +5,7 @@
 show databases; 
  use raj;
  show tables;
- drop table authors;drop table books;
+ -- drop table authors;drop table books;
  create table Authors(
  author_id bigint primary key,
  first_name varchar(100) not null,
@@ -15,7 +15,7 @@ show databases;
  );
  
   create table Books(
- book_id bigint primary key,
+ book_id bigint auto_increment primary key,
  title varchar(100) not null,
  author_id bigint,
  publication_year year,
@@ -25,4 +25,24 @@ show databases;
  );
  
  -- Insert three authors into the Authors table with different nationalities
- 
+ insert into Authors(author_id, first_name, last_name, birth_year,nationality)
+ values(101,"jaun","elia",1948,"indian");
+insert into Authors values(105,"william","deo",1953,"foreignor");
+insert into Authors values(109,"jubin","nautiyal",1998,"indian");
+
+-- viewing the records
+select*from Authors;
+-- Insert five books into the Books table, ensuring at least two books are by the same author
+INSERT INTO Books (title, author_id, publication_year, genre, price, stock) 
+VALUES
+('Python Basics',101,2020,'Programming',20.00,10);
+INSERT INTO Books (title, author_id, publication_year, genre, price, stock) 
+VALUES('Python advance',101,2021,'Programming',20.00,10);
+INSERT INTO Books (title, author_id, publication_year, genre, price, stock) 
+VALUES('java script',105,2022,'languages',20.00,10);
+INSERT INTO Books (title, author_id, publication_year, genre, price, stock) 
+VALUES('English',105,2021,'languages',20.00,10);
+INSERT INTO Books (title, author_id, publication_year, genre, price, stock) 
+VALUES('Mysql',109,2025,'Programming',20.00,10);
+
+select*from Books;
